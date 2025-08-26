bf214dd3558f699bdff9b79f933c765e98e40ae8
Pedro de Souza <131737607+Pedro-OSouza@users.noreply.github.com>
2025-08-25 21:08:55 -0300

    main app javascript file; added the news api file and modular files closer and auto dismiss

--- scripts/javascript/app.js
+++ scripts/javascript/app.js
@@ -1,3 +1,6 @@
+import autoDismiss from './modules/autoDismiss/autoDismiss.js';
+import closer from './modules/closer/closer.js';
+
 // Carrega módulos conforme a página
 if (document.querySelector('.btn-presenca')) {
     import('./modules/turmas/presencaUi.js');
@@ -6,3 +9,14 @@ if (document.querySelector('.btn-presenca')) {
 if (document.querySelector('.button-criar-aula')) {
     import('./modules/turmas/aulaUi.js')
 }
+
+if(document.querySelector(".toggle-closer") && document.querySelector(".toggle-closer-btn")){
+    closer('.toggle-closer', '.toggle-closer-btn')
+
+}
+
+if(document.querySelector('.auto-dismiss')){
+    autoDismiss('.auto-dismiss')
+}
+
+/* Todo: ADicionar o css com fade-out effect */
